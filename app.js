8354912d9e28f1cf44059fe859e9062a3dcca0ea
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-21 21:57:56 +0000

    :card_file_box: connecting to db

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 
 const app = express();
+
+
+// Connecting to DB
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log('connected to db')
+);
+
 
 // Routes
 app.get('/', (req,res) => {
     res.send('Home page')
 });
+
 
 // Listening to the server
 app.listen(3000);
