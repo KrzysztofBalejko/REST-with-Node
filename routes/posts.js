9956980a11de352422ed2da41a94eaab6ba185a5
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-21 22:37:32 +0000

    :building_construction: further changes to routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const Post = require('../models/Post');
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
     res.send('Posts page');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
 });
 
 module.exports = router;
