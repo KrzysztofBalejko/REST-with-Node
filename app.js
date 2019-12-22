d82eca149ca00a1d9c9f6c5feaac5c87065fb8c0
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-22 16:08:49 +0000

    :package: importing cors

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-
+app.use(cors());
 app.use(bodyParser.json());
 
 // Importing routes
